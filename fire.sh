0bd7598c36331a3bab3e3bcfae239a6524d9d150
iserikov <igor@serikov.ru>
2019-06-03 13:09:29 +0300

    install of mc, docker added to hostprep role

--- fire.sh
+++ fire.sh
@@ -1,5 +1,5 @@
-ansible-playbook -i $PWD/inventory/inventory.yml   -u laborant  --key-file=~/.ssh/ioserikov $PWD/playbooks/keydeploy.yml --extra-vars '{"pubkey":"ioserikov.pub", "username":"laborant", "pathdir":"~/.ssh/"}'
+#ansible-playbook -i $PWD/inventory/inventory.yml   -u laborant  --key-file=~/.ssh/ioserikov $PWD/playbooks/keydeploy.yml --extra-vars '{"pubkey":"ioserikov.pub", "username":"laborant", "pathdir":"~/.ssh/"}'
 
-ansible-playbook  -i $PWD/inventory/inventory.yml -u laborant --key-file=~/.ssh/ioserikov   $PWD/playbooks/addrepos.yml  --extra-vars '{"pathdir":"/home/iserikov/crobundle/dev/src/MyProjes/fabric_ansible_1.4/vault/","src":"repos"}'
+#ansible-playbook  -i $PWD/inventory/inventory.yml -u laborant --key-file=~/.ssh/ioserikov   $PWD/playbooks/addrepos.yml  --extra-vars '{"pathdir":"/home/iserikov/crobundle/dev/src/MyProjes/fabric_ansible_1.4/vault/","src":"repos"}'
 
-ansible-playbook -i $PWD/inventory/inventory.yml  -u laborant  --key-file=~/.ssh/ioserikov $PWD/playbooks/updupgr.yml
+ansible-playbook -i $PWD/inventory/inventory.yml  -u laborant  --key-file=~/.ssh/ioserikov $PWD/playbooks/raf_start.yml
