a3b63360916fa5d7d3e58f3cf7afe5d8c4f5ff70
iserikov <igor@serikov.ru>
2019-06-19 20:35:49 +0300

    fabric ca server

--- fire.sh
+++ fire.sh
@@ -1,4 +1,4 @@
-#ansible-playbook -i $PWD/inventory/inventory.yml   -u laborant  --key-file=~/.ssh/ioserikov $PWD/playbooks/keydeploy.yml --extra-vars '{"pubkey":"ioserikov.pub", "username":"laborant", "pathdir":"~/.ssh/"}'
+#ansible-playbook -i $PWD/inventory/inventory.yml   -u laborant  --ask-pass $PWD/playbooks/keydeploy.yml --extra-vars '{"pubkey":"ioserikov.pub", "username":"laborant", "pathdir":"~/.ssh/"}'
 
 #ansible-playbook  -i $PWD/inventory/inventory.yml -u laborant --key-file=~/.ssh/ioserikov   $PWD/playbooks/addrepos.yml  --extra-vars '{"pathdir":"/home/iserikov/crobundle/dev/src/MyProjes/fabric_ansible_1.4/vault/","src":"repos"}'
 
